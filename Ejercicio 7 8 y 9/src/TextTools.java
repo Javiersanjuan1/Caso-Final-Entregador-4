ae5c98c42fb2eacea1f1e0f0f2cd5ea111c9a5c6
Javiersanjuan1 <fsanjua@myuax.com>
2024-04-09 19:00:42 +0200

    Initial commit

--- Ejercicio 7 8 y 9/src/TextTools.java
+++ Ejercicio 7 8 y 9/src/TextTools.java
@@ -4,7 +4,7 @@ import java.awt.event.*;
 import java.io.*;
 import java.util.*;
 
-public cla TextTools {
+public class TextTools {
 
     private static final Map<String, JFrame> documentWindows = new HashMap<>();
 
